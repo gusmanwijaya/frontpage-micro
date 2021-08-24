f4d2ff6345535558973faf749a245d4c9adffd05
Gusman Wijaya <gusman.w.jaya@gmail.com>
2021-08-24 17:54:49 +0700

    Slicing Photos

--- src/pages/courses/[id].js
+++ src/pages/courses/[id].js
@@ -10,6 +10,7 @@ import { CSSTransition } from "react-transition-group";
 import formatThousand from "src/helpers/formatThousand";
 import { useEffect, useRef, useState } from "react";
 import Footer from "src/parts/Footer";
+import CoursePhoto from "src/parts/Details/CoursePhoto";
 
 function DetailsCourse({ data }) {
   const footer = useRef(null);
@@ -151,6 +152,22 @@ function DetailsCourse({ data }) {
               <p className="text-gray-600 text-lg leading-relaxed mb-3">
                 {data?.description ?? "No Description Found"}
               </p>
+            </section>
+
+            <section className="mt-10">
+              <h6 className="font-medium text-gray-900 text-2xl mb-4">
+                Course <span className="text-teal-500">Photos</span>
+              </h6>
+
+              <div className="flex justify-start items-center -mx-4 mt-6">
+                {data?.images?.length > 0 ? (
+                  data?.images?.map?.((photo, index) => (
+                    <CoursePhoto data={photo.image} key={index} />
+                  ))
+                ) : (
+                  <div className="w-full text-center py-12">No Item Found</div>
+                )}
+              </div>
             </section>
           </div>
         </div>
